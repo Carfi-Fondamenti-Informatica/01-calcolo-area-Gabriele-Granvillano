7c276e1c660fa15484ba2b2d1a829a1dd63bb222
Gabriele-Granvillano <114691186+Gabriele-Granvillano@users.noreply.github.com>
2022-11-10 18:30:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-//Qui vanno definite le variabili
-
-cin >> a >> b >> c;
-
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+int main()
+{
+  float a, b, c;
+  cin >> a >> b >> c;
+  cout << (a*b)/2 << endl;
+  cout << a*a << endl;
+  cout << a*b << endl;
+  cout << ((b+a)*c)/2 << endl;
+  return 0;
 }
